811c7721026bef0b504978a0ac581cd556d177cb
Md Saiful Islam <mdsaifulislam@Mds-MacBook-Air.local>
2024-02-18 19:55:00 +0200

    final touch

--- scripts/banner.js
+++
@@ -1,8 +0,0 @@
-
-// Get the modal element
-
-
-// Display the modal when the coupon is successfully applied
-// Replace this with your actual logic for applying the coupon and displaying the modal
-// For example:
-
